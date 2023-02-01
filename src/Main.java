25de19a842a5ffdb6d9f14dbcb6d27bc32b118d7
binmek94th <binyamm83@gmail.com>
2023-02-01 17:38:42 +0300

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args) {
+        String language = "java";
+        language = "C++";
+        System.out.println(language);
+        System.out.print("O ");
+        System.out.print("X ");
+        System.out.println('X');
+        System.out.print("O ");
+        System.out.print("X ");
+        System.out.println('O');
+        System.out.print("X ");
+        System.out.print("O ");
+        System.out.println('X');
+    }
+}
